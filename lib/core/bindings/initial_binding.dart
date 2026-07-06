a26ecb045b42ee43e786b51a90a6fba3b1d7e984
Kazan'an <damife0160@gmail.com>
2026-07-06 23:59:28 +0100

    werty

---
+++ lib/core/bindings/initial_binding.dart
@@ -0,0 +1,8 @@
+import 'package:get/get.dart';
+
+class InitialBinding extends Bindings {
+  @override
+  void dependencies() {
+    // Inject global dependencies here (e.g., NetworkInfo, GetConnect, SharedPrefs, etc.)
+  }
+}
---
+++ lib/core/routes/app_routes.dart
@@ -0,0 +1,4 @@
+abstract class AppRoutes {
+  static const String home = '/home';
+  // Add more routes here as features grow
+}
