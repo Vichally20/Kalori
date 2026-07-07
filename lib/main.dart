5e19950375bd27facd85e87d5e383fda72b00842
Kazan'an <damife0160@gmail.com>
2026-07-07 23:03:52 +0100

    project set-up part1

--- lib/main.dart
+++ lib/main.dart
@@ -1,1 +1,28 @@
-// my name is stan
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'core/bindings/initial_binding.dart';
+import 'core/routes/app_pages.dart';
+import 'core/routes/app_routes.dart';
+
+void main() {
+  runApp(const KaloriApp());
+}
+
+class KaloriApp extends StatelessWidget {
+  const KaloriApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return GetMaterialApp(
+      title: 'Kalori',
+      debugShowCheckedModeBanner: false,
+      initialBinding: InitialBinding(),
+      initialRoute: AppRoutes.home,
+     // getPages: AppPages.pages,
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.green),
+        useMaterial3: true,
+      ),
+    );
+  }
+}
