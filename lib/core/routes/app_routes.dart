6fbe3a3a6e2d52c09a7f682169c58949ec08d6ae
Kazan'an <damife0160@gmail.com>
2026-07-13 23:51:14 +0100

    route settings and cleanups

--- lib/core/routes/app_routes.dart
+++ lib/core/routes/app_routes.dart
@@ -2,5 +2,18 @@ abstract class AppRoutes {
   static const String home = '/home';
   static const String profile = '/profile';
   static const String log = '/log';
+  static const String history = '/history';
+  static const String settings = '/settings';
+  static const String notifications = '/notifications';
+  static const String helpAndFeedback = '/helpAndFeedback';
+  static const String account = '/account';
+  static const String subscriptions = '/subscriptions';
+  static const String privacyPolicy = '/privacyPolicy';
+  static const String termsOfService = '/termsOfService';
+  static const String integrations = '/integrations';
+  static const String insights = '/insights';
+  static const String waterTracker = '/waterTracker';
+  static const String foodLibrary = '/foodLibrary';
+  
   // Add more routes here as features grow
 }
