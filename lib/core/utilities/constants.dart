0f07b3e9929a4a10b8021b4dbd5f101e803543fa
Kazan'an <damife0160@gmail.com>
2026-07-13 23:54:15 +0100

    kalori early constants

---
+++ lib/core/utilities/constants.dart
@@ -0,0 +1,11 @@
+class Constants {
+  static const String appTitle = 'Kalori';
+  static const String appVersion = '1.0.0';
+  static const String appDescription =
+      'Kalori is a calorie tracking application that helps you track your daily calorie intake and provides insights into your diet.';
+  static const String appAuthor = 'Kalori Team';
+  static const String appLicense = 'MIT';
+  static const String appWebsite = 'https://kalori.app';
+  static const String appSupportEmail = "[EMAIL_ADDRESS]";
+  static const String appSupportUrl = 'https://kalori.app/support';
+}
